b020b51d486953db5a24f1545b34ac46ffeb6c9e
bhoobalan <38600241+bhoobal@users.noreply.github.com>
2019-06-08 11:55:29 +0530

    Update import-data.sh

--- import-data.sh
+++ import-data.sh
@@ -1,5 +1,5 @@
 #wait for the SQL Server to come up
-sleep 90s
+sleep 90
 
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1wontT3ll# -d master -i setup.sql
