e727b0ed1fdf58ee34e427abc3328582fe20ad78
bhoobalan <38600241+bhoobal@users.noreply.github.com>
2019-06-08 12:39:41 +0530

    Update import-data.sh

--- import-data.sh
+++ import-data.sh
@@ -1,5 +1,5 @@
 #wait for the SQL Server to come up
-sleep 90
+sleep 180
 #run the setup script to create the DB and the schema in the DB
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 1wontT3ll# -d master -i setup.sql
 #import the data from the csv file
